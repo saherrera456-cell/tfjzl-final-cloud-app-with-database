7ede49087e082bfa80b5392f844853d1af01fdbf
Santiago <saherrera456@gmail.com>
2026-05-18 15:31:17 -0500

    Task 2: admin.py actualizado con Inlines y modelos registrados

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,39 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+# Importación de las 7 clases solicitadas
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
-
-
+# Configuración de Inlines para edición en cascada
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
-# Register your models here.
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+# Clases Admin personalizadas
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-
-# <HINT> Register Question and Choice models here
-
+# Registro de todos los modelos en el sitio de administración
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
